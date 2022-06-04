11c7e3a9e4a0f941fdca8c4a860d0a5e1afaebe5
seeratawan01 <seeratsdsking@gmail.com>
2022-06-04 12:24:03 +0500

    feat: responsive navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+</script>
+
+
+<template>
+  <header class="navbar bg-base-100 border-b py-2.5">
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case font-bold text-xl">Ticket<span class="text-primary">Pass</span></a>
+    </div>
+    <nav class="flex-none">
+
+      <div class="dropdown dropdown-end sm:hidden">
+        <label tabindex="0" class="btn btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <RouterLink to="/" custom v-slot="{ navigate }">
+              <button @click="navigate" @keypress.enter="navigate" role="link" class="btn btn-primary rounded-full text-white px-12">Create Event</button>
+            </RouterLink>
+          </li>
+
+          <li class="mt-2.5">
+            <RouterLink to="/login" custom v-slot="{ navigate }">
+              <button @click="navigate" @keypress.enter="navigate" role="link"  class="btn btn-outline px-12">Login</button>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+
+
+      <ul class="hidden sm:flex menu menu-horizontal p-0">
+        <li>
+          <RouterLink to="/" custom v-slot="{ navigate }">
+            <button @click="navigate" @keypress.enter="navigate" role="link" class="btn btn-primary rounded-full text-white px-12">Create Event</button>
+          </RouterLink>
+        </li>
+
+        <li class="ml-2.5">
+          <RouterLink to="/login" custom v-slot="{ navigate }">
+            <button @click="navigate" @keypress.enter="navigate" role="link"  class="btn btn-outline px-12">Login</button>
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#34cc98",
+          secondary: "#effbf6",
+          accent: "#1FB2A6",
+          neutral: "#1B1C22",
+          "base-100": "#FFFFFF",
+          info: "#4CBBD6",
+          success: "#1F937C",
+          warning: "#DA8F16",
+          error: "#DA253D",
+
+          "--rounded-btn": "4rem",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
